f23f0690b015b04c5d3756818652954df948b1cf
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-02-21 00:52:42 -0800

    Cleanup

--- update_index.sh
+++ update_index.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-# find $HOME/Downloads /mnt/data -type d | sort | sed '/\/\.[^\.]*/d' > ~/.dir_index.txt
+# Disable globbing
+set -f
 
+# Acquire lists of directories
 dirs=$(find "$HOME/Downloads" "/mnt/data" -type d | sort | sed '/\/\.[^\.]*/d')
 freq_dirs=$(cat "$HOME/.dir_frequent.txt" | sort -r | sed 's/^\([0-9]*\)\t\(.*\)/\2/')
 
-IFS=$'\n'       # make newlines the only separator
-set -f          # disable globbing
-
 rm "$HOME/.dir_index.txt"
 
-#for line in $(cat < "$HOME/.dir_frequent.txt"); do
-#    freq_dir=$(echo "$line" | cut -f2 -d$'\t')
-#    echo "$freq_dir" >> "$HOME/.dir_index.txt"
-#done
-
+# Write most frequent directories
 while read -r dir; do
 	echo "$dir" >> "$HOME/.dir_index.txt"
 done <<< "$freq_dirs"
 
+# Write other directories
 while read -r dir; do
 	is_freq=false
 	while read -r freq_dir; do
-		#IFS=$'\t' read -ra items <<< "$line"
-		#freq_dir="${items[1]}"
 		if [ "$dir" == "$freq_dir" ]; then is_freq=true; break; fi
 	done <<< "$freq_dirs"
 
