04c7efe0d331cd04558397d2282ea8922f33d2b7
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-02-24 00:00:07 -0800

    Generalized to more than just directory list

--- update_freq.sh
+++ update_freq.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-# Updates frequency list with given directory
-# Usage: update_freq "/home/user/Desktop"
+# Updates given frequency list with given item
+# Usage: update_freq "$HOME/.dir_frequent.txt" "$HOME/Desktop"
+# Usage: update_freq "$HOME/.emoji_frequent.txt" ":)"
 
-dir="$1"
-path="$HOME/.dir_frequent.txt"
-path_tmp="$HOME/.dir_frequent.txt.1"
+path="$1"
+input_item="$2"
 freq=$(cat "$path")
 
-# Increment frequency count for given directory
+# Increment frequency count for given item
 is_found=false
 while read -r line; do
 	IFS=$'\t' read -ra items <<< "$line"
 	freq_count="${items[0]}"
-	freq_dir="${items[1]}"
+	freq_item="${items[1]}"
 
-	if [ "$freq_dir" == "$dir" ]; then
+	if [ "$freq_item" == "$input_item" ]; then
 		let "freq_count=freq_count+1"
-		line="$freq_count	$freq_dir"
+		line="$freq_count	$freq_item"
 		is_found=true
 	fi
 
-	echo "$line" >> "$path_tmp"
+	echo "$line" >> "$path.1"
 done <<< "$freq"
 
-# Create entry for given directory if necessary
+# Create entry for given item if necessary
 if ! "$is_found"; then
-	echo "1	$dir" >> "$path_tmp"
+	echo "1	$input_item" >> "$path.1"
 fi
 
 # Sort and update
-sort -o "$path_tmp" "$path_tmp"
-mv "$path_tmp" "$path"
+sort -o "$path.1" "$path.1"
+mv "$path.1" "$path"
 
 # TODO Frecency
 
