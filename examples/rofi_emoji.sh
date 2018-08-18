b0db80f0f0aefcff018c67e39bb16d203ab22739
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-08-18 05:09:48 -0700

    Emojis now work perfectly

--- examples/rofi_emoji.sh
+++ examples/rofi_emoji.sh
@@ -3,6 +3,7 @@ root_dir=$(dirname "$0")"/.."
 "$root_dir/list_index.sh" ~/.emoji_index.txt ~/.emoji_frequent.txt | \
     rofi "$@" -dmenu | \
     (read x; [[ -z $x ]] || (
-        gio open "$x"
+        emoji=$(cut -d$'\t' -f1 <<<"$x" | sed -e 's/[[:space:]]*$//')
+        echo -n "$emoji" | xclip -selection clipboard
         "$root_dir/update_freq.sh" ~/.emoji_frequent.txt "$x"
         "$root_dir/update_index.sh" ~/.emoji_index.txt ~/.emoji_frequent.txt "$x"))
