3c0469a6d23eb94a58e60917095e0ef1971830ea
Frank Steinborn <steinex@nognu.de>
2018-09-22 23:13:17 +0200

    add shebang to rofi-emjoi.sh; use version 11 emojis

--- examples/refresh_emoji_index.sh
+++ examples/refresh_emoji_index.sh
@@ -3,7 +3,7 @@
 root_dir=$(dirname "$0")"/.."
 path_index="$HOME/.emoji_index.txt"
 path_freq="$HOME/.emoji_frequent.txt"
-url="http://www.unicode.org/Public/emoji/5.0/emoji-test.txt"
+url="http://www.unicode.org/Public/emoji/11.0/emoji-test.txt"
 
 curl -s "$url" | sed 's/^[^#]*; fully-qualified *# \([^ ]*\)/\1 \t/gp;d' > "$path_index.1"
 
