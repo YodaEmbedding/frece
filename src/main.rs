9f4fe5800997336ca35448efd17c4b836fc76aa7
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-05-09 01:36:21 -0700

    Refactor a bit

--- src/main.rs
+++ src/main.rs
@@ -38,9 +38,9 @@ impl fmt::Display for Field {
 }
 
 fn frecency(count: i64, secs: i64) -> f64 {
-    let x = 0.0
-        + 0.25 * (1.0 + count as f64).ln()
-        - 0.30 * (1.0 + secs  as f64).ln();
+    let x = 1.0
+        + 0.08 * (1.0 + count as f64).ln()
+        - 0.10 * (1.0 + secs  as f64).ln();
 
     1.0 / (1.0 + (-x).exp())
 }
@@ -71,16 +71,6 @@ fn init_db(now: DateTime<Utc>) -> Result<()> {
     Ok(())
 }
 
-// TODO generic type?
-fn db_read_fields() -> Result<Vec<Field>> {
-    let db_str = fs::read_to_string("db.txt")?;
-    let fields = db_str.lines()
-        .map(parse_line)
-        .collect::<Result<Vec<Field>>>()?;
-
-    Ok(fields)
-}
-
 // fn sort_fields<I>(<...>) -> Result<...>
 //
 // fn my_func<T: AsRef<str>>(list: &[T]) {
@@ -98,41 +88,38 @@ fn db_read_fields() -> Result<Vec<Field>> {
 //     }
 // }
 
-// TODO return value...? or rename to "db_print_sorted?"
-fn db_sorted(now: DateTime<Utc>) -> Result<()> {
-    let mut fields = db_read_fields()?;
+fn sort_by_frecency(fields: &mut [Field], now: DateTime<Utc>) {
     fields.sort_by_cached_key(|x| -x.frecency(&now));
+}
 
-    let sorted_str = fields.iter()
+fn to_name_str(fields: &[Field]) -> String {
+    fields.iter()
         .map(|x| x.data.as_ref())
         .collect::<Vec<&str>>()
-        .join("\n");
-
-    println!("{}", sorted_str);
-
-    Ok(())
+        .join("\n")
 }
 
-fn db_info(now: DateTime<Utc>) -> Result<()> {
-    // TODO SORT???
-
-    let db_str = fs::read_to_string("db.txt")?;
-    for line in db_str.lines() {
-        let field = parse_line(line)?;
-        let secs = now.signed_duration_since(field.time).num_seconds();
-        println!("{:.6} {:6} {} {}",
-                 frecency(field.count, secs),
-                 field.count, secs, field.data);
-    }
-
-    Ok(())
+fn to_info_str(fields: &[Field], now: DateTime<Utc>) -> String {
+    fields.iter()
+        .map(|field| {
+            let secs = now.signed_duration_since(field.time).num_seconds();
+            format!("{:.6}  {:6}  {:25}  {}",
+                    frecency(field.count, secs),
+                    field.count,
+                    field.time.to_rfc3339_opts(SecondsFormat::Secs, false),
+                    field.data)
+        })
+        .collect::<Vec<String>>()
+        .join("\n")
 }
 
-// TODO might be better that these functions only read the whole file once...
-// And don't recompute lines/lengths/etc...
-fn increment_db(line: usize) -> Result<()> {
-    let mut fields = db_read_fields()?;
-    fields[line].count += 1;  // TODO is this really needed? why not just make new var...
+fn increment_db(fields: &[Field], now: DateTime<Utc>, line: usize) -> Result<()> {
+    let field = Field::new(
+        fields[line].count + 1,
+        now,
+        fields[line].data.clone());
+
+    let field_str = field.to_string();
 
     let mut file = OpenOptions::new()
         .read(true)
@@ -142,8 +129,7 @@ fn increment_db(line: usize) -> Result<()> {
     let mut file_str = String::new();
     file.read_to_string(&mut file_str)?;
 
-    let lines = file_str.lines()
-        .collect::<Vec<&str>>();
+    let lines = file_str.lines().collect::<Vec<&str>>();
 
     // TODO just zip this with fields? or make it a part of Field? idk...
     // maybe just calculate directly from each field's string length...
@@ -153,12 +139,12 @@ fn increment_db(line: usize) -> Result<()> {
         .collect::<Vec<usize>>();
 
     // Check if line swap required
-    if line == 0 || fields[line - 1].count >= fields[line].count {
+    if line == 0 || fields[line - 1].count >= field.count {
         let seek_pos = |x| lengths.iter().take(x).sum::<usize>() + x;
         let seek_begin = seek_pos(line);
         let seek_end   = seek_pos(line + 1);
 
-        let write_str = fields[line].to_string();
+        let write_str = field.to_string();
         assert!(write_str.len() == seek_end - seek_begin - 1);
 
         file.seek(SeekFrom::Start(seek_begin as u64))?;
@@ -169,7 +155,7 @@ fn increment_db(line: usize) -> Result<()> {
 
     // TODO this could be done instead of explicit if check above?
     let line1 = fields[..line].iter()
-        .rposition(|x| x.count >= fields[line].count)
+        .rposition(|x| x.count >= field.count)
         .map(|x| x + 1)
         .unwrap_or(0);
     let line2 = line;
@@ -178,9 +164,7 @@ fn increment_db(line: usize) -> Result<()> {
     let seek_begin = seek_pos(line1);
     let seek_end   = seek_pos(line2 + 1);
 
-    let s = fields[line].to_string();
-    let top = iter::once(&s[..]);  // TODO wat?
-    // let top = iter::once(lines[line2]);
+    let top = iter::once(field_str.as_str());
     let middle = lines.iter()
         .skip(line1 + 1)
         .take(line2 - line1 - 1)
@@ -204,12 +188,21 @@ fn increment_db(line: usize) -> Result<()> {
 fn main() -> Result<()> {
     let now = Utc::now();
 
+    let db_str = fs::read_to_string("db.txt")?;
+    let lines = db_str.lines().collect::<Vec<&str>>();
+    let fields = lines.iter()
+        .map(|&x| parse_line(x))
+        .collect::<Result<Vec<Field>>>()?;
+
+    let mut sorted_fields = fields.iter()
+        .map(|x| x.clone())
+        .collect::<Vec<Field>>();
+
     // init_db(now)?;
-    db_sorted(now)?;
-    // swap_db(0, 8)?;
-    db_info(now)?;
-    increment_db(5)?;
-    // Write db (increment and swap if needed?)
+    increment_db(&fields, now, 5)?;
+    sort_by_frecency(&mut sorted_fields, now);
+    println!("{}", to_name_str(&sorted_fields));
+    println!("{}", to_info_str(&sorted_fields, now));
 
     Ok(())
 }
@@ -220,3 +213,5 @@ fn main() -> Result<()> {
 // TODO file::create automatically
 // TODO might be more elegant/faster to sort in reverse order?
 // TODO reduce code duplication... and make proper pure functions and file access!
+// TODO allow frecency weights to be tweaked?
+// TODO DbWriter: writes only invalidated entries? ...too complicated for a simple program, dude...
