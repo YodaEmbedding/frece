e41bb2a9a4d324ab748b71130fdb1bf37dab180f
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-05-16 00:14:10 -0700

    Added Travis CI (auto-deploy release binaries)

---
+++ ci/before_deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+CARGO=cargo
+
+set -ex
+
+build() {
+    "$CARGO" build --target "$TARGET" --verbose
+
+    # Output most recent build error
+    set +x
+    stderr="$(find "target/$TARGET/release" -name stderr -print0 |
+        xargs -0 ls -t | head -n1)"
+    if [ -s "$stderr" ]; then
+        echo "===== $stderr ====="
+        cat "$stderr"
+        echo "====="
+    fi
+    set -x
+}
+
+make_tarball() {
+    local name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
+    local temp_dir="$(mktemp -d)"
+    local stage_dir="$temp_dir/$name"
+    local out_dir="$(pwd)/deploy"
+    mkdir -p "$out_dir"
+
+    cp "target/$TARGET/release/frece" "$stage_dir/"
+    cp LICENSE "$stage_dir/"
+    cp README.md "$stage_dir/"
+
+    (cd "$temp_dir" && tar czf "$out_dir/$name.tar.gz" "$name")
+    rm -rf "$temp_dir"
+}
+
+main() {
+    build
+    make_tarball
+}
+
+main
