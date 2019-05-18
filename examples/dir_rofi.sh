c0580adf64cd16718172cb4f3d2769e46734f63f
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-05-17 17:32:51 -0700

    Use read -r to avoid losing backslashes

--- examples/dir_rofi.sh
+++ examples/dir_rofi.sh
@@ -4,6 +4,6 @@ DB_FILE="$HOME/.frece_dir.db"
 
 frece print "$DB_FILE" | \
     rofi "$@" -dmenu | \
-    (read x; [[ -z $x ]] || (
+    (read -r x; [[ -z $x ]] || (
         gio open "$x"
         frece increment "$DB_FILE" "$x"))
--- examples/emoji_rofi.sh
+++ examples/emoji_rofi.sh
@@ -4,7 +4,7 @@ DB_FILE="$HOME/.frece_emoji.db"
 
 frece print "$DB_FILE" | \
     rofi "$@" -dmenu | \
-    (read x; [[ -z $x ]] || (
+    (read -r x; [[ -z $x ]] || (
         emoji=$(cut -d$'\t' -f1 <<<"$x" | sed -e 's/[[:space:]]*$//')
         echo -n "$emoji" | xclip -selection clipboard
         frece increment "$DB_FILE" "$x"))
