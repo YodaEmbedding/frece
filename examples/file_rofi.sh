ae3bd8edfca8e8b523ce807ed5883c1bdca99466
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-09-10 17:37:04 -0700

    exit with errorcode (1)

--- examples/file_rofi.sh
+++ examples/file_rofi.sh
@@ -3,7 +3,7 @@
 DB_FILE="$HOME/.frece_file.db"
 
 item=$(frece print "$DB_FILE" | rofi "$@" -dmenu)
-[[ -z $item ]] && exit
+[[ -z $item ]] && exit 1
 frece increment "$DB_FILE" "$item"
 
 xdg-open "$item"
