25fc642f7bd154cae23ac0f6e89560870c0639ea
Sven Kube <git@sven-kube.de>
2021-08-24 00:39:36 +0200

    feat: change data directory and create it if it does not exist for file and dir example

--- examples/file_update.sh
+++ examples/file_update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-DB_FILE="$HOME/.frece/db/file.db"
+DATA_DIR="$HOME/.config/frece"
+DB_FILE="$DATA_DIR/file.db"
 ENTRIES_FILE="/tmp/frece_file_entries.txt"
 
 find "$@" -path '*/\.*' -prune -o -not -name '.*' -type f -print | \
     sort > "$ENTRIES_FILE"
+
+[ ! -d "$DATA_DIR" ] && mkdir -p "$DATA_DIR"
 
 if [ ! -f "$DB_FILE" ]; then
     frece init "$DB_FILE" "$ENTRIES_FILE"
