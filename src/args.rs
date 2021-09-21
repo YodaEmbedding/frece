62669dee1024ad5e2dfc49ca91fad25361a041fa
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-09-21 12:46:48 -0700

    chore: bump version number

--- src/args.rs
+++ src/args.rs
@@ -10,7 +10,7 @@ pub fn get_matches<'a>() -> clap::ArgMatches<'a> {
         .required(true);
 
     App::new("frece")
-        .version("1.0.4")
+        .version("1.0.5")
         .author("Mateen Ulhaq <mulhaq2005@gmail.com>")
         .about("Frecency indexed database")
         .subcommand(
